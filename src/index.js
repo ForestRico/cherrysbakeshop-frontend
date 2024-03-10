f06abb867ffb07b59257ebd46124f089156fb070
ForestRico <126733661+ForestRico@users.noreply.github.com>
2024-03-10 22:35:56 +1100

    fixed frontend deployment configuration

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { BrowserRouter } from 'react-router-dom';
 import CartProvider from './hooks/useCart';
 import './axiosConfig';
 
-// import { AuthProvider } from './hooks/useAuth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
